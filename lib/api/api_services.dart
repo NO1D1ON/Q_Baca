1fc2d697c4d84b43f14b48a9a23622c931043ba0
NO1D1ON <228160012@students.uma.ac.id>
2025-07-02 23:11:05 +0700

    NDK

--- lib/api/api_services.dart
+++ lib/api/api_services.dart
@@ -21,7 +21,7 @@ class ApiService {
 
   // Gunakan 'http://10.0.2.2:8000' jika menggunakan Emulator Android.
   // Ganti sesuai IP Anda jika menjalankan di HP fisik.
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrl = 'https://qbaca.my.id';
 
   ApiService() {
     dio.options.baseUrl = '$baseUrl/api';
